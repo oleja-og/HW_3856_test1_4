2ae2df976bcf377eaa7bcdbbdb21d98b3671c2fc
oleja-og <oleja.og@gmail.com>
2023-03-10 01:01:45 +0300

    added solution

---
+++ final_work/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует массив из строк,
+// длинна которых меньше либо равна 3 символа. 
+
+
+string[] massiv1 = {"hello","2","world",":-)"};
+string[] massiv2 = {"1234","1567","-2","computer science"};
+string[] massiv3 = {"Russia","Denmark","Kazan"};
+
+
+string[] EnterArray(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length<=3)
+        {
+           count++;
+        }
+    }
+    
+    string[] massiv = new string[count];
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length<=3)
+        {
+           massiv[j] = array[i];
+           j++;
+        }
+    }
+    return massiv;
+}
+
+
+void Tusk()
+{
+        string[] massiv = EnterArray(massiv1);
+        Console.WriteLine("[{0}]", string.Join(",",massiv));
+        Console.WriteLine();
+        massiv = EnterArray(massiv2);
+        Console.WriteLine("[{0}]", string.Join(",",massiv));
+         Console.WriteLine();
+        massiv = EnterArray(massiv3);
+        Console.WriteLine("[{0}]", string.Join(",",massiv));
+}
+
+
+Tusk();
